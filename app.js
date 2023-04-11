3ff1865ab1a42d3307bbdf28bab55d2ca311d28c
PixiePOP404 <asli.ayse.sahin.404@gmail.com>
2023-04-11 22:52:25 +0300

    error fixing attempt 2

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require("path");
 
 const app = express();
 app.set("view engine", "ejs");
-app.set("views");
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "/views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("index");
