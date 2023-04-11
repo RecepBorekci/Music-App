53b1084f2c4233d118c49dde0fe308d72b70bf51
PixiePOP404 <asli.ayse.sahin.404@gmail.com>
2023-04-11 22:39:10 +0300

    error fix attempt

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require("path");
 
 const app = express();
 app.set("view engine", "ejs");
+app.set("views");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -20,5 +21,3 @@ app.get("/profile", function (req, res) {
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
-
-app.set("views");
