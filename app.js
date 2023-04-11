5d3360fd1f903d33e432db3e7f9666eabfc15f6f
PixiePOP404 <asli.ayse.sahin.404@gmail.com>
2023-04-11 22:22:56 +0300

    index.ejs configuration

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("index");
+  res.render("../index");
 });
 app.get("/playlist", function (req, res) {
   res.render("playlist");
