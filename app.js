845a21efa1953e81490143d81f091f01452a6491
RecepBorekci <recepborekci29@gmail.com>
2023-04-19 21:26:01 +0300

    redesign auth pages, refactor codes and files

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get("/callback", function (req, res) {
   res.redirect("/");
 });
 
+app.get("/reallogin", function(req, res) {
+  res.render("login.ejs");
+})
+
 const client_id = "30d5140203ce42c88337910fc2b6aef1";
 const client_secret = "b214294c05ef41debf2ba2f0cbc8b8c7";
 var redirect_uri = "http://localhost:3000/callback";
