af81a099dec233e592b31be81cdf0e8ac1608755
RecepBorekci <recepborekci29@gmail.com>
2023-04-25 13:45:11 +0300

    add login, signup and logout functionality

--- app.js
+++ app.js
@@ -26,7 +26,22 @@ app.get("/callback", function (req, res) {
 
 app.get("/reallogin", function(req, res) {
   res.render("login.ejs");
-})
+});
+
+// Possible login functionality. Not yet implemented.
+
+// app.post("/reallogin", function(req, res) {
+
+//     var email = req.body.email;
+//     var password = req.body.password;
+
+//     console.log("The email is: " + email);
+//     console.log("The password is: " + password);
+
+//     res.redirect("/");
+
+// });
+
 
 const client_id = "30d5140203ce42c88337910fc2b6aef1";
 const client_secret = "b214294c05ef41debf2ba2f0cbc8b8c7";
