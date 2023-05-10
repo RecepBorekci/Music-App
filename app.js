fe29437ddb6084e881ef43283bc66e1240d8419d
PixiePOP404 <asli.ayse.sahin.404@gmail.com>
2023-05-10 17:55:59 +0300

    Profile and playlists for an added user

--- app.js
+++ app.js
@@ -10,96 +10,56 @@ app.set("views", __dirname + "/views");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+var user = {};
+var playlists = {};
+const client_id = "30d5140203ce42c88337910fc2b6aef1";
+const client_secret = "b214294c05ef41debf2ba2f0cbc8b8c7";
+const redirect_uri = "http://localhost:3000/callback";
+const stateKey = "spotify_auth_state";
+var back_url = "/";
+
 app.get("/", function (req, res) {
   res.render("index.ejs");
 });
 
 app.get("/playlist", function (req, res) {
-  res.render("playlist.ejs");
+  try {
+    res.render("playlist.ejs", { items: playlists.items });
+  } catch (error) {
+    back_url = "/playlist";
+    res.redirect("/login");
+  }
 });
 
 app.get("/profile", function profile(req, res) {
-  //fetchProfile();
-  res.render("profile.ejs");
+  try {
+    res.render("profile.ejs", {
+      profilePicture:
+        user.images.length != 0
+          ? user.images[0].url
+          : "https://static8.depositphotos.com/1009634/988/v/450/depositphotos_9883921-stock-illustration-no-user-profile-picture.jpg",
+      username: user.display_name,
+      email: user.email,
+      followers: user.followers.total,
+    });
+  } catch (error) {
+    console.log("Cannot go to profile page. Redirecting to login page.");
+    back_url = "/profile";
+    res.redirect("/login");
+  }
 });
 
 app.get("/callback", function (req, res) {
   const code = req.query.code || null;
-
-  axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    data: querystring.stringify({
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: redirect_uri,
-    }),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${client_id}:${client_secret}`
-      ).toString("base64")}`,
-    },
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        const { access_token, token_type } = response.data;
-        axios
-          .get("https://api.spotify.com/v1/me", {
-            headers: {
-              Authorization: `${token_type} ${access_token}`,
-            },
-          })
-          .then((response) => {
-            res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-          })
-          .catch((error) => {
-            res.send(error);
-          });
-      } else {
-        res.send(response);
-      }
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-  //res.redirect("/");
+  fetchData(code);
+  res.redirect(back_url);
 });
-
-app.get("/refresh_token", (req, res) => {
-  const { refresh_token } = req.query;
-
-  axios({
-    method: "post",
-    url: "https://accounts.spotify.com/api/token",
-    data: querystring.stringify({
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
-    }),
-    headers: {
-      "content-type": "application/x-www-form-urlencoded",
-      Authorization: `Basic ${new Buffer.from(
-        `${client_id}:${client_secret}`
-      ).toString("base64")}`,
-    },
-  })
-    .then((response) => {
-      res.send(response.data);
-    })
-    .catch((error) => {
-      res.send(error);
-    });
-});
-
-const client_id = "30d5140203ce42c88337910fc2b6aef1";
-const client_secret = "b214294c05ef41debf2ba2f0cbc8b8c7";
-var redirect_uri = "http://localhost:3000/callback";
-const stateKey = "spotify_auth_state";
 
 app.get("/login", function (req, res) {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
-  var scope = "user-read-private user-read-email";
+  var scope =
+    "user-read-private user-read-email playlist-read-private playlist-read-collaborative";
 
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
@@ -113,57 +73,67 @@ app.get("/login", function (req, res) {
   );
 });
 
-async function fetchToken() {
-  let token;
-  await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
+async function fetchData(code) {
+  fetchToken(code)
+    .then((response) => {
+      if (response.status === 200) {
+        const { access_token, token_type } = response.data;
+        axios
+          .get("https://api.spotify.com/v1/me", {
+            headers: {
+              Authorization: `${token_type} ${access_token}`,
+            },
+          })
+          .then((response) => {
+            //user = `${JSON.stringify(response.data, null, 2)}`;
+            user = response.data;
+            userId = user.id;
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        axios
+          .get(`https://api.spotify.com/v1/users/${userId}/playlists`, {
+            headers: {
+              Authorization: `${token_type} ${access_token}`,
+            },
+            params: {
+              limit: 50,
+            },
+          })
+          .then((response) => {
+            playlists = response.data;
+            console.log(playlists.items);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        console.log(response);
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+async function fetchToken(code) {
+  return axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: redirect_uri,
+    }),
     headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
+      "content-type": "application/x-www-form-urlencoded",
       Authorization: `Basic ${new Buffer.from(
         `${client_id}:${client_secret}`
       ).toString("base64")}`,
     },
-    body:
-      "grant_type=client_credentials&client_id=" +
-      encodeURIComponent(client_id) +
-      "&client_secret=" +
-      encodeURIComponent(client_secret),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      token = data.access_token;
-      console.log(token);
-    });
-
-  return token;
-}
-
-async function fetchProfile() {
-  let token = await fetchToken();
-  const res = await fetch(`https://api.spotify.com/v1/me`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-    method: "GET",
   });
-  let result = await res.json();
-  console.log(result);
-  return result;
 }
-
-// async function fetchWebApi(endpoint, method, body) {
-//   let token = fetchToken();
-//   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
-//     headers: {
-//       Authorization: `Bearer ${token}`,
-//     },
-//     method,
-//     body: JSON.stringify(body),
-//   });
-//   let result = await res.json();
-//   console.log(result);
-//   return result;
-// }
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
